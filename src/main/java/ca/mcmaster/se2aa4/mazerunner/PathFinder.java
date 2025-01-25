09310439c51fd3214591cd7ac66e26ff46b40013
ahmedz-5645 <ahmed.zafar5645@gmail.com>
2025-01-24 23:59:38 -0500

    added direction enum, and pathfinder class

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -0,0 +1,85 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.util.ArrayList;
+
+public class PathFinder {
+    public ArrayList<Integer> pos;
+    public StringBuffer path;
+    public Direction direct;
+
+    
+    public PathFinder (){
+        this.path = new StringBuffer("");
+        this.pos = new ArrayList<Integer>();
+        this.direct = Direction.RIGHT;
+    }
+
+    public ArrayList<Integer> startAtEntry(ArrayList<ArrayList<Character>> grid){
+        ArrayList<Integer> entry = new ArrayList<Integer>();
+        for (int i = 0; i < (grid.get(0)).size(); i++){
+            ArrayList<Character> row = grid.get(i);
+            if (row.get(0).equals(' ')){
+                
+                entry.add(i);
+                entry.add(0);
+                this.pos.set(0, i);
+                this.pos.set(1, 0);
+                return entry;
+            }
+        }
+        //if no entry found, return empty position
+        return entry;
+    }
+
+    public boolean checkIfExit (ArrayList<ArrayList<Character>> grid){
+        if (this.pos.get(1) == ((grid.get(0)).size() - 1)){
+            if ((grid.get(this.pos.get(0)).get(this.pos.get(1))) == ' '){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void movePos(char character, ArrayList<ArrayList<Character>> grid){
+        if (character == 'F'){
+            if ((this.direct == Direction.UP) && (grid.get(this.pos.get(0) + 1).get(this.pos.get(1)) != '#')){
+                int temp = this.pos.get(0);
+                temp++;
+                this.pos.set(0, temp);
+            } else if ((this.direct == Direction.RIGHT) && (grid.get(this.pos.get(0)).get(this.pos.get(1) + 1) != '#')){
+                int temp = this.pos.get(1);
+                temp++;
+                this.pos.set(1, temp);
+            } else if ((this.direct == Direction.DOWN) && (grid.get(this.pos.get(0) - 1).get(this.pos.get(1)) != '#')){
+                int temp = this.pos.get(0);
+                temp--;
+                this.pos.set(0, temp);
+            } else {
+                int temp = this.pos.get(1);
+                temp--;
+                this.pos.set(1, temp);
+            }
+        } else if (character == 'R') {
+            if (this.direct == Direction.RIGHT){
+                this.direct = Direction.DOWN;
+            } else if (this.direct == Direction.DOWN){
+                this.direct = Direction.LEFT;
+            } else if (this.direct == Direction.LEFT){
+                this.direct = Direction.UP;
+            } else {
+                this.direct = Direction.RIGHT;
+            }
+        } else {
+            if (this.direct == Direction.RIGHT){
+                this.direct = Direction.UP;
+            } else if (this.direct == Direction.DOWN){
+                this.direct = Direction.RIGHT;
+            } else if (this.direct == Direction.LEFT){
+                this.direct = Direction.DOWN;
+            } else {
+                this.direct = Direction.LEFT;
+            }
+        }
+    }
+}
